2929c99e1607cf4079dedbe5ed1b749846783ba6
maahiunnati <74832066+maahiunnati@users.noreply.github.com>
2021-10-03 19:01:36 +0530

    Create FibonacciNumberCode.pp

---
+++ FibonacciNumberCode.pp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int fibon(int n)
+{
+    int a = 0, b = 1, c, i;
+    if( n == 0)
+        return a;
+    for(i = 2; i <= n; i++)
+    {
+       c = a + b;
+       a = b;
+       b = c;
+    }
+    return b;
+}
+ 
+// Driver code
+int main()
+{
+    int n = 7;//let we want seventh fibonacci number
+     
+    cout << fibon(n);
+    return 0;
+}
