20eaea03bc42dd95fe406dae4a6fa23e6121979b
A. Santhosh Kumar <58947968+Santhoshstark06@users.noreply.github.com>
2021-10-05 13:37:00 +0530

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -1,24 +1,16 @@
-#include <stdio.h>
-
-int main()
-{
-    int count, first = 0, second = 1, next, i;
-    
-    printf("Number of terms: \n");
-    scanf("%d",&count);
-    printf("First %d Fibonacci numbers are: /n", count);
-    for(i = 0; i < count;  i++)
-    {
-        if(i <= 1)
-            next = i;
-        else
-        {
-            next = first + second;
-            first = second;
-            second = next;
-        }
-        printf("%d\n", next);
-    }
-    
-    return 0;
-}
+#include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,i,number;    
+ printf("Enter the number of elements:");    
+ scanf("%d",&number);    
+ printf("\n%d %d",n1,n2);//printing 0 and 1    
+ for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+ }    
